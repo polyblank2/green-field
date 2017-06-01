6144330cb643c9656b2d4c26c97188ac60d81252
Mosher <polyblank2@gmail.com>
2017-06-01 13:17:40 -0700

    Generates name in style similar to Heroku
    I added Horse Names. 
    Modified from being console based to printing to the page based on Vanilla JavaScript

---
+++ docs/NameGenerator/generateName.js
@@ -0,0 +1,42 @@
+function generateName()
+{
+adjs = [
+    "autumn", "hidden", "bitter", "misty", "silent", "empty", "dry", "dark",
+    "summer", "icy", "delicate", "quiet", "white", "cool", "spring", "winter",
+    "patient", "twilight", "dawn", "crimson", "wispy", "weathered", "blue",
+    "billowing", "broken", "cold", "damp", "falling", "frosty", "green",
+    "long", "late", "lingering", "bold", "little", "morning", "muddy", "old",
+    "red", "rough", "still", "small", "sparkling", "throbbing", "shy",
+    "wandering", "withered", "wild", "black", "young", "holy", "solitary",
+    "fragrant", "aged", "snowy", "proud", "floral", "restless", "divine",
+    "polished", "ancient", "purple", "lively", "nameless"
+  ]
+  nouns = ["Abbey","Ace","Aesop","Afrika","Aggie","Ajax","Alpha","Alfie","Ali","Aladdin","Alibaba","Ally","America","Amber","Amos","Amigo","Andy","Annabelle","Annapolis","Angel","Annie","Ansel","Apache","Applejack","Apollo","April","Arabella","Ariel","Aries","Arnold","Arod","Arrow","Arso","Aspen","Augustus","Aussi","Austin","Avenger","Avis","Axel","Ayasha","Babe","Baby","Ballerina","Bam","Bam","Bambi","Bandera","Bandit","Bandita","Bandy","Banjo","Banter","Barbie","Baxter","Bear","Beauty","BeeBee","Bella","Belle","Bello","Benbow","Bender","Biscuits","Bishop","Birdie","Blackie","Blackjack","Blaze","Blossom","Blue","Blue","boy","Blue","Moon","Bo","Boaz","Bodhi","Bogart","Bonnie","Booker","Boomer","Boon","Boots","Beauregard","Boris","Bowie","Brady","Brandy","Brazos","Breeze","Brigadier","Buck","Buckshot","Bud","Buddy","Budweiser","Bullet","Buster","Buttons","Butter","Buttermilk","Butterscotch","Buzz","Cabaret","Calamity","Calypso","Caliber","Candy","Cannon","Cappicino","Captain","Carson","Casanova","Casey","Cash","Casino","Casper","Cecil","Champ","Champion","Chance","Charcoal","Charley","Horse","Checkers","Chett","Cherry","Chester","Chex","Cheyenne","Chi","Chica","Chief","Chip","Chippewa","Chipper","Chix","Chloe","Choctaw","Chodak","Cinnamon","Cisco","Cloud","Clover","Clyde","Cochise","Coco","Cocolo","Cole","Conan","Concho","Cookie","Cooper","Copper","Corazon","Corky","Cory","Country","Cowboy","Crazy","Horse","Cricket","Cruiser","Cupid","Cutter","Cyrus","Czar","Daisy","Dakota","Dallas","Dan","Dancer","Dandy","Darrion","Dash","Dawn","Dealer","Deeno","Defiance","Delmonico","Denby","Derringer","Desi","Desperado","Dewy","Dexter","Dezior","Diablo","Diamond","Diesel","Digby","Digger","Dillon","Dillinger","Dina","Dinero","Dino","Disby","Disco","Diva","Dixie","Dolla","Dollar","Dolly","Dominic","Dominator","Dora","Dorado","Drake","Dream","Dreamer","Drifter","Duce","Duchess","Duke","Dunny","Durango","Duster","Dusty","Easter","Ebony","Echo","Eclipse","Eddie","Eldorado","Eleazar","Eli","Elixir","Ellie","Elvis","Ember","Epona","Esperanza","Esteban","Excalibur","Fancy","Fargo","Felise","Festus","Fiddle","Fifty","Fiona","Flash","Fletch","Flicka","Freckles","Frida","Friday","Frisky","Gambler","Gantry","Gator","Gemini","Genie","Genius","Gent","Gentry","George","Georgia","Ghost","Gidget","Gilly","Gin","Ginger","Giselle","Goldie","Goliath","Gracie","Grit","Guapo","Gucci","Gulliver","Gunner","Gus","Gypsy","Hana","Hanna","Hardy","Harley","Harvey","Hashtag","Hawk","Hercules","Hero","Hickory","Hobo","Holiday","Holly","Holy","Smoke","Hombre","Hondo","Honey","Honey","Dew","Hooch","Hoover","Hopscotch","Hoss","Houdini","Howdy","Huck","Huckleberry","Huey","Hurricane","Indiana","Indigo","Indy","Invitation","Ira","Ivan","Ivanhoe","Ivery","Jack","Jackie","Jagger","Jake","Jarvis","Jasmine","Jasper","Jazzibelle","Jazzy","Jed","Jetta","Jenny","Jerico","Jesso","Jester","Jet","Jewel","Jiminey","Jingles","Jo","Joey","Jo","Kid","Jonah","Johan","Joni","Joy","Jule","Jumper","Juno","Justice","Kansas","Kate","Katy","Brown","Keisha","Kemosabe","Keno","Kendra","Kia","Kickapoo","Kietche","King","Kiowa","Kipper","Kit","Kodiak","Kody","Lacey","Lady","Lakota","Legend","Legacy","Lena","Levi","Leo","Lexy","Liberty","Lightning","Lily","Lita","Lobo","Lola","Lonesome","Loredo","Lucero","Lucky","Lucy","Luke","Lumpy","Luna","Mac","Maggie","Magic","Magnum","Malachi","Malak","Marco","Marengo","Mariah","Marty","Marz","Master","Mateo","Max","Maya","Memphis","Belle","Merlyn","Mesa","Mica","Micky","Midnight","Milly","Mingo","Minnie","Minot","Miracle","Mischief","Missy","Misty","Mixer","Mocha","Molly","Money","Montana","Monty","Moon","Moondance","Moonshine","Moose","Mordecai","Morgan","Moxie","Mystic","Mystery","Nacho","Nala","Natacha","Navajo","Nemo","Neptune","Nero","Nevada","Night","Niner","Nyx","Oliver","Ollie","Oncore","Onyx","Opal","Oreo","Outlaw","Ozzy","Paco","Pablo","Paige","Paisley","Panama","Pandora","Papoose","Paprika","Partner","Patches","Patriot","Paiute","Pawnee","Payday","Peaches","Pebbles","Pecos","Penny","Pepper","Peso","Pharoah","Philippe","Picalo","Picasso","Pie","Pierce","Pierre","Pilot","Pistol","Pixy","Playboy","Playgirl","Play","Gun","Pocahontas","Pocket","Pogo","Polo","Poncho","Poppy","Prairie","Prancer","Prankster","Presley","Pride","Prince","Professor","Promise","Prowler","Pueblo","Queball","Queen","Queenie","Quervo","Quest","Quincy","Rafter","Rai","Rain","Rainy","Rambo","Ranger","Rascal","Raven","Razor","Razuli","Reba","Rebel","Red","Reggie","Reilly","Rembrant","Remi","Remington","Renegade","Reno","Rhapsody","Rhythm","Ricochet","Riddle","Rifle","Ringo","Rio","Riptide","Ritz","River","Robby","Rocky","Rodeo","Rojo","Rolly","Roman","Rono","Rooster","Rounder","Rowdy","Rowen","Roy","Ruby","Rumi","Rumor","Rustler","Rusty","Ruth","Sabino","Sabrina","Sage","Sahara","Sailor","Saint","Sally","Salty","Sammy","Sampson","Sandy","Sargent","Sassy","Savanna","Scamper","Scarlet","Scooner","Scotty","Scout","Secret","Seeka","Seminole","Serenade","Serendipity","Sgt","Pepper","Shadow","Shayla","Shawnee","Shieka","Shiloh","Shooter","Shorty","Shoshone","Sidekick","Silver","Sinatra","Skeeter","Skippy","Sky","Smoke","Smokey","Snickers","Snoopy","Soldier","Solomon","Song","Sooner","Sophi","Spanky","Sparrow","Spirit","Splash","Spook","Spot","Sport","Spring","Sputnic","Stan","Star","Stella","Stepper","Stewy","Stix","Stomp","Stormy","Stretch","Strut","Stumpy","Summer","Sundance","Sunday","Sunny","Surprise","Taco","Tandy","Tango","Tank","Tanka","Tarzan","Tesa","Tessy","Theia","Theo","Theodore","Thor","Thunder","Tiki","Tilley","Tinker","Tiny","Tim","Tipper","Tippy","Titan","Titus","Toby","Tokahee","Tomahawk","Tonto","Tootie","Topaz","Tornado","Torro","Tow","Bar","Traveler","Travis","Treasure","Trevor","Trickster","Trigger","Trinket","Troubadour","Trucker","Trusty","Tucker","Tuff","Turbo","Twister","Ty","Umber","Ulysses","Uno","Uri","Utah","Val","Van","Gogh","Vargas","Vegas","Venus","Vesta","Victory","Wapiti","Warrior","Waylon","Weaver","Wes","Whinney","Whisper","Wicked","Wildfire","Wiley","Willard","Willie","Willow","Winchester","Windy","Wing","Winston","Winter","Wolf","Wrangler","Xavier","Yakama","Yankie","Yeller","Yeti","Yoda","Yonkers","Zahara","Zara","Zelda","Zenia","Zia","Zipper","Zodiac","Zoe","Zoey","Zoro","Zeus","Zuza",
+    "waterfall", "river", "breeze", "moon", "rain", "wind", "sea", "morning",
+    "snow", "lake", "sunset", "pine", "shadow", "leaf", "dawn", "glitter",
+    "forest", "hill", "cloud", "meadow", "sun", "glade", "bird", "brook",
+    "butterfly", "bush", "dew", "dust", "field", "fire", "flower", "firefly",
+    "feather", "grass", "haze", "mountain", "night", "pond", "darkness",
+    "snowflake", "silence", "sound", "sky", "shape", "surf", "thunder",
+    "violet", "water", "wildflower", "wave", "water", "resonance", "sun",
+    "wood", "dream", "cherry", "tree", "fog", "frost", "voice", "paper",
+    "frog", "smoke", "star"
+  ]
+  rnd_adj = Math.floor(Math.random()*adjs.length)
+  rnd_noun = Math.floor(Math.random()*nouns.length)
+  rnd_num = rnd = Math.floor(Math.random()*Math.pow(2,12))
+  //"#{adjs[rnd>>6%64]}_#{nouns[rnd%64]}"
+  
+  function capitalize(string) 
+  {
+      return string.charAt(0).toUpperCase() + string.slice(1);
+  }
+  
+  var first=adjs[rnd_adj];
+  var last=nouns[rnd_adj];
+  
+  first = capitalize(first);
+  last = capitalize(last);
+  
+	return (first+"_"+last+"_"+rnd_num);
+}
